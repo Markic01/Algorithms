97fe45b15c6682edc42a4375ce2f0a50e064fbdf
Marko Radosavljevic <01marko.radosavljevic@gmail.com>
2021-07-06 23:54:15 +0200

    Switched to dynamically allocated memory instead of statically

--- Floyd Warshall.cpp
+++ Floyd Warshall.cpp
@@ -1,10 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
+#define MEM_ERR "Not enough memory!"
 #define pb push_back
-int x[10000][10000],n,m,d[10000][10000],pre[10000][10000];
+
+int n,m;
 vector<int> c;
-void put(int i,int j){
-    if(pre[i][j]==-1){
+
+void error(string s){
+	cout<<s<<'\n';
+	exit(EXIT_FAILURE);
+}
+
+int** allocate_matrix(int n,int m){
+	int** x=(int**)malloc(n*sizeof(*x));
+	if(x==NULL)
+		error(MEM_ERR);
+	for(int i=0;i<n;i++){
+		x[i]=(int*)malloc(m*sizeof(**x));
+		if(x[i]==NULL){
+			for(int j=i-1;j>=0;j--){
+				free(x[i]);
+				x[i]=NULL;
+			}
+			free(x);
+			x=NULL;
+			error(MEM_ERR);
+		}
+	}
+	return x;
+}
+
+void path(int i,int j,int** prev){
+    if(prev[i][j]==-1){
         c.pb(i);
         while(c.size()>1&&c[c.size()-1]==c[c.size()-2])c.pop_back();
         c.pb(j);
@@ -13,34 +41,40 @@ void put(int i,int j){
     }
     c.pb(i);
     while(c.size()>1&&c[c.size()-1]==c[c.size()-2])c.pop_back();
-    put(i,pre[i][j]);
-    put(pre[i][j],j);
+    path(i,prev[i][j],prev);
+    path(prev[i][j],j,prev);
     c.pb(j);
     while(c.size()>1&&c[c.size()-1]==c[c.size()-2])c.pop_back();
 }
+
 int main(){
+	int n,m;
     cin>>n>>m;
+    int **x,**d,**prev;
+    x=allocate_matrix(n,n);
+    d=allocate_matrix(n,n);
+    prev=allocate_matrix(n,n);
     for(int i=0;i<n;i++)
         for(int j=0;j<n;j++)
-            x[i][j]=0,d[i][j]=99;
+            x[i][j]=0,d[i][j]=INT_MAX;
     int a,b,t;
     for(int i=0;i<m;i++){
         cin>>a>>b>>t;
         a--;
         b--;
         x[a][b]=d[a][b]=t;
-        pre[a][b]=-1;
-        pre[b][a]=-1;
+        prev[a][b]=-1;
+        prev[b][a]=-1;
         x[b][a]=d[b][a]=t;
     }
     for(int i=0;i<n;i++)
         for(int j=0;j<n;j++)
             for(int k=0;k<n&&j!=i;k++)
                 if(d[i][j]+d[j][k]<d[i][k]&&j!=k&&i!=k)
-                    d[i][k]=d[i][j]+d[j][k],pre[i][k]=j;
+                    d[i][k]=d[i][j]+d[j][k],prev[i][k]=j;
     int u,v;
     cin>>u>>v;
-    put(u,v);
-    for(int i=0;i<c.size();i++)
+    path(u,v,prev);
+    for(int i=0;i<(int)c.size();i++)
         cout<<c[i]<<" ";
 }
