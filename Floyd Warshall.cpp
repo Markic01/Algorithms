c930be8ce22b0a1a26289478592141f704cbac8c
Marko Radosavljevic <01marko.radosavljevic@gmail.com>
2021-07-17 18:55:34 +0200

    maintenance

--- Floyd Warshall.cpp
+++ Floyd Warshall.cpp
@@ -6,29 +6,29 @@
 
 void path(int from,int to,const std::vector<std::vector<int>>& prev,std::vector<int>& sol){
     if(sol.size()==0||sol[sol.size()-1]!=from)
-		sol.push_back(from);
+        sol.push_back(from);
     
-	if(prev[from][to]!=-1){
-		path(from,prev[from][to],prev,sol);
-		path(prev[from][to],to,prev,sol);
-	}
+    if(prev[from][to]!=-1){
+        path(from,prev[from][to],prev,sol);
+        path(prev[from][to],to,prev,sol);
+    }
 	
     if(sol.size()==0||sol[sol.size()-1]!=to)sol.push_back(to);
 }
 
 void FloydWarshall(const int nodes, std::vector<std::vector<int>>& dist, std::vector<std::vector<int>>& prev){
-	for(int beg=0;beg<nodes;beg++)
+    for(int beg=0;beg<nodes;beg++)
         for(int mid=0;mid<nodes;mid++)
-			if(mid!=beg&&dist[beg][mid]!=INT_MAX)
-				for(int end=0;end<nodes;end++)
-					if(dist[mid][end]!=INT_MAX&&beg!=end&&mid!=end&&dist[beg][mid]+dist[mid][end]<dist[beg][end]){
-						dist[beg][end]=dist[beg][mid]+dist[mid][end];
-						prev[beg][end]=mid;
-					}
+            if(mid!=beg&&dist[beg][mid]!=INT_MAX)
+                for(int end=0;end<nodes;end++)
+                    if(dist[mid][end]!=INT_MAX&&beg!=end&&mid!=end&&dist[beg][mid]+dist[mid][end]<dist[beg][end]){
+                        dist[beg][end]=dist[beg][mid]+dist[mid][end];
+                        prev[beg][end]=mid;
+                    }
 }
 
 int main(){
-	int nodes,vertices;
+    int nodes,vertices;
     std::cin>>nodes>>vertices;
 
     std::vector<std::vector<int>> dist(nodes,std::vector<int>(nodes));
@@ -38,7 +38,7 @@ int main(){
         for(int j=0;j<nodes;j++)
             dist[i][j]=INT_MAX;
         dist[i][i]=0;
-	}
+    }
 
     int pointA,pointB,length;
     for(int i=0;i<vertices;i++){
@@ -49,7 +49,7 @@ int main(){
     
     FloydWarshall(nodes,dist,prev);
     
-	std::vector<int> sol;
+    std::vector<int> sol;
     int from,to;
     std::cin>>from>>to;
     path(from,to,prev,sol);
