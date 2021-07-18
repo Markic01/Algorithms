c84a0ff7c75f7e39303ac25415e7d876449c96b8
Marko Radosavljevic <01marko.radosavljevic@gmail.com>
2021-07-18 18:11:58 +0200

    maintenance

--- Articulation Points.cpp
+++ Articulation Points.cpp
@@ -1,45 +1,56 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define pb push_back
-#define N 100000
-int n,m,disc[N],low[N],parent[N];
-static int k,tim,visited[N],art_point[N];
-vector<int> x[N];
-int dfs(int i,int p){
-    parent[i]=p;
-    disc[i]=low[i]=tim++;
-    visited[i]=1;
+#include <iostream>
+#include <vector>
+
+void dfs(int i, int currParent, const std::vector<std::vector<int>>& adjacent, 
+         std::vector<int>& parent, std::vector<int>& discovered, std::vector<int>& lowest,
+         std::vector<bool>& isVisited, std::vector<bool>& isArticulationPoint){
+
+    static int startTime=0;
+    parent[i]=currParent;
+    discovered[i]=lowest[i]=startTime++;
+    isVisited[i]=true;
     int children=0;
-    for(int j=0;j<x[i].size();j++)
-        if(!visited[x[i][j]]){
-            dfs(x[i][j],i);
+
+    for(int j=0;j<(int)adjacent[i].size();j++){
+        if(!isVisited[adjacent[i][j]]){
+            dfs(adjacent[i][j],i,adjacent,parent,discovered,lowest,isVisited,isArticulationPoint);
             children++;
-            low[i]=min(low[i],low[x[i][j]]);
+            lowest[i]=std::min(lowest[i],lowest[adjacent[i][j]]);
             if(parent[i]==-1&&children>1)
-                art_point[i]=1;
-            if(parent[i]!=-1&&low[x[i][j]]>=disc[i])
-                art_point[i]=1;
+                isArticulationPoint[i]=1;
+            if(parent[i]!=-1&&lowest[adjacent[i][j]]>=discovered[i])
+                isArticulationPoint[i]=1;
         }
-        else
-            low[i]=min(low[i],disc[x[i][j]]);
-}
-int main(){
-    cin>>n>>m;
-    int a,b;
-    for(int i(0);i<m;i++){
-        cin>>a>>b;
-        x[a].pb(b);
-        x[b].pb(a);
+        else{
+            lowest[i]=std::min(lowest[i],discovered[adjacent[i][j]]);
+        }
     }
-    dfs(0,-1);
-    cout<<"Articulation points are: ";
-    for(int i=0;i<n;i++)
-        if(art_point[i])
-            cout<<i<<" ";
-    cout<<'\n';
-    for(int i(0);i<n;i++)
-        cout<<disc[i]<<" ";
-    cout<<'\n';
-    for(int i(0);i<n;i++)
-        cout<<low[i]<<" ";
+}
+
+int main(){
+    int nodes,edges;
+    std::cin>>nodes>>edges;
+
+    std::vector<std::vector<int>> adjacent(nodes);
+    std::vector<int> discovered(nodes),lowest(nodes),parent(nodes,-1);
+    std::vector<bool> isVisited(nodes,false),isArticulationPoint(nodes,false);
+
+    int from,to;
+    for(int i=0;i<edges;i++){
+        std::cin>>from>>to;
+        adjacent[from].push_back(to);
+        adjacent[to].push_back(from);
+    }
+
+    dfs(0,-1,adjacent,parent,discovered,lowest,isVisited,isArticulationPoint);
+
+    std::cout<<"Articulation points are: ";
+    for(int i=0;i<nodes;i++){
+        if(isArticulationPoint[i]){
+            std::cout<<i<<" ";
+        }
+    }
+    std::cout<<'\n';
+    
+    exit(EXIT_SUCCESS);
 }
