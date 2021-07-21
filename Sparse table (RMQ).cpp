e13546bf8bbd5c57ebf18855dfa78c4f246ab60e
Marko Radosavljevic <01marko.radosavljevic@gmail.com>
2021-07-21 16:11:52 +0200

    making the code readable

--- Sparse table (RMQ).cpp
+++ Sparse table (RMQ).cpp
@@ -1,32 +1,45 @@
-#include<bits/stdc++.h>
-using namespace std;
-static int sparse[100000][20],n,x[100000];
-int mah(int i,int j){
-    if(x[sparse[i][j-1]]<x[sparse[i+(1<<(j-1))][j-1]])
-        return sparse[i][j-1];
-    return sparse[i+(1<<(j-1))][j-1];
-}
-int query(int l,int d){
-    int a=d-l+1;
-    int r=log2(a);
-    if(x[sparse[l][r]]<x[sparse[d-(1<<r)+1][r]])
-        return sparse[l][r];
-    return sparse[d-(1<<r)+1][r];
-}
-int main(){
-    cin>>n;
-    int l=log2(n)+1;
-    for(int i=0;i<n;i++)cin>>x[i],sparse[i][0]=i;
-    for(int j=1;j<l;j++)
-        for(int i=0;i<(n-(1<<j)+1);i++)
-            sparse[i][j]=mah(i,j);//,cout<<i<<" "<<j<<" "<<(n-(1<<j)+1)<<endl;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<l;j++)
-            cout<<sparse[i][j]<<" ";
-        cout<<endl;
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+
+int minHalf(const std::vector<int>& inArray, std::vector<std::vector<int>>& sparseTable,const int i,const int j){
+    if(inArray[sparseTable[i][j-1]]<inArray[sparseTable[i+(1<<(j-1))][j-1]]){
+        return sparseTable[i][j-1];
     }
-    cout<<"koji je query?"<<endl;
-    int a,b;
-    cin>>a>>b;
-    cout<<x[query(a,b)];
+    return sparseTable[i+(1<<(j-1))][j-1];
+}
+
+int query(const std::vector<int>& inArray, const std::vector<std::vector<int>>& sparseTable,const int left,const int right){
+    int lengthOfSubarray=right-left+1;
+    int logLength=log2(lengthOfSubarray);
+    if(inArray[sparseTable[left][logLength]]<inArray[sparseTable[right-(1<<logLength)+1][logLength]]){
+        return sparseTable[left][logLength];
+    }
+    return sparseTable[right-(1<<logLength)+1][logLength];
+}
+
+int main(){
+    int numElements;
+    std::cin>>numElements;
+    int maxSubarrays=log2(numElements)+1;
+
+    std::vector<std::vector<int>> sparseTable(numElements,std::vector<int>(maxSubarrays));
+    std::vector<int> inArray(numElements);
+
+    for(int i=0;i<numElements;i++){
+        std::cin>>inArray[i];
+        sparseTable[i][0]=i;
+    }
+
+    for(int j=1;j<maxSubarrays;j++){
+        for(int i=0;i<(numElements-(1<<j)+1);i++){
+            sparseTable[i][j]=minHalf(inArray,sparseTable,i,j);
+        }
+    }
+
+    int leftIndex,rightIndex;
+    std::cin>>leftIndex>>rightIndex;
+    std::cout<<inArray[query(inArray,sparseTable,leftIndex,rightIndex)]<<'\n';
+    exit(EXIT_SUCCESS);
 }
